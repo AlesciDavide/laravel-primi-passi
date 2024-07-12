427ee15294ea969f46b8d1e1847e5f6ba1ae8adc
AlesciDavide <davi.alesci@Gmail.com>
2024-07-12 13:50:48 +0200

    stapato a pagina dinamicamente

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $home = 'Benvenuto nella home page!!';
+
+    return view('home', compact("home"));
 });
